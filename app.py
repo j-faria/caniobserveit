bf8bae6fb219bd6dc80cc97334227ba761532c7d
João Faria <joao.faria@astro.up.pt>
2018-04-30 00:22:10 -0300

    simple form

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import request, render_template
 
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('search-form.html')
 
 
-@app.route('/<star>')
-def hello_star(star):
-    return "Hello {}!".format(star)
+# @app.route('/<star>')
+# def hello_star(star):
+#     return "Hello {}!".format(star)
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
 
 if __name__ == '__main__':
     app.run()
